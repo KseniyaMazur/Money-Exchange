13af86f30ab390179438eb15711b52c19a09f72e
KseniyaMazur <ksuha80018@gmail.com>
2018-02-27 16:10:14 +0300

    Money-Exchangee/feature: implement makeExchange function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    
+    var money = {
+        H: 0,
+        Q: 0,
+        D: 0,
+        N: 0,
+        P: 0,
+    }
+    if (currency > 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    };
+
+    while( currency > 0){
+        if (currency >= 50){
+            currency -= 50;
+            money.H++;
+        } else if (currency < 50 && currency >= 25) {
+            currency -= 25;
+            money.Q++;
+        } else if (currency < 25 && currency >= 10) {
+            currency -= 10;
+            money.D++;
+        } else if (currency < 10 && currency >= 5) {
+            currency -= 5;
+            money.N++;
+        } else if (currency < 5 && currency >= 1) {
+            currency -= 1;
+            money.P++;
+        }
+    };
+
+    for (let nameMoney in money) {
+        if (money[nameMoney] === 0) {
+          delete money[nameMoney];
+        }
+    };
+
+    return money;
+    
 }
